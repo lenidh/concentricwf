9ea68e31640f5daf16b74e5c509b101992ff40a0
Moritz Heindl <lenidh@gmail.com>
2024-02-04 18:54:54 +0100

    Set targetSdk to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     defaultConfig {
         applicationId = "de.lenidh.concentricwf"
         minSdk = 30
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "0.1"
         vectorDrawables {
